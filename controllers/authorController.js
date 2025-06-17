063e1474327ada6c944cd709358e4d94a3f408d8
SaraToth <saratoth101@gmail.com>
2025-06-17 17:41:41 +0900

    Feat: Update errors in authorController to custom

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,12 +1,12 @@
 const db = require("../db");
 const asyncHandler = require("express-async-handler");
+const CustomNotFoundError = require("../errors/CustomNotFoundError");
 
 const getAuthorById = asyncHandler(async (req, res) => {
     const { authorId } = req.params;
     const author = await db.getAuthorById(Number(authorId));
     if (!author) {
-        res.status(404).send("Author not found");
-        return;
+        throw new CustomNotFoundError("Author not found");
     }
     res.send(`Author Name: ${author.name}`);
 
@@ -16,8 +16,7 @@ const getAuthors = asyncHandler(async (req, res) => {
     const authors = await db.getAuthors();
 
     if (!authors) {
-        res.status(404).send("No authors available");
-        return;
+        throw new CustomNotFoundError("No Authors available");
     }
     res.send(authors);
 });
