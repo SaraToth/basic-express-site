59410d1d05b98dd08b898fb32aed0d9a89ae44ef
SaraToth <saratoth101@gmail.com>
2025-06-17 17:18:02 +0900

    Style: Simplify app code

--- app.js
+++ app.js
@@ -13,17 +13,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log("routing");
 });
-
-
-/*GET /about
-GET /contact
-POST /contact
-
-GET /books
-GET /books/:bookId
-GET /books/:bookId/reserve
-POST /books/:bookId/reserve
-
-GET /authors
-GET /authors/:authorId
-*/
