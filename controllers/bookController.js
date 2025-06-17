2de94ff7f0c23faaae98c1de47f3bb089f27d7cb
SaraToth <saratoth101@gmail.com>
2025-06-17 17:08:28 +0900

    Feat: Create books database and controllers

---
+++ controllers/bookController.js
@@ -0,0 +1,25 @@
+const db = require("../db");
+
+async function getBooks(req, res) {
+    const books = await db.getBooks();
+
+    if (!books) {
+        res.status(404).send("No books are available");
+    };
+
+    res.send(books);
+};
+
+async function getBookById(req, res) {
+    const { bookId } = req.params;
+
+    const book = await db.getBookById(Number(bookId));
+
+    if (!book) {
+        res.status(404).send("Book not found");
+    }
+
+    res.send(`Book title: ${book.title}`);
+};
+
+module.exports = { getBookById, getBooks };
--- db.js
+++ db.js
@@ -4,9 +4,19 @@ const authors = [
     { id: 3, name: "Jason" },
 ];
 
+const books = [
+    { id: 1, title: "Harry Potter" },
+    { id: 2, title: "Christmas in New York" },
+    { id: 3, title: "On the Road" },
+];
+
 async function getAuthorById(authorId) {
     return authors.find(author => author.id === authorId);
 }; 
+
+async function getBookById(bookId) {
+    return books.find(book => book.id === bookId);
+};
 
 async function getAuthors() {
     const authorList = [];
@@ -14,4 +24,10 @@ async function getAuthors() {
     return authorList.join(", ");
 };
 
-module.exports = { getAuthorById, getAuthors };
+async function getBooks() {
+    const bookList = [];
+    books.forEach(book => bookList.push(book.title));
+    return bookList.join(", ");
+};
+
+module.exports = { getAuthorById, getAuthors, getBooks, getBookById };
