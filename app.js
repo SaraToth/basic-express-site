8f2d55a9dad06cfddd3a202ff8ea99ffac1e7580
SaraToth <saratoth101@gmail.com>
2025-06-17 17:32:05 +0900

    Feat: Add error handling middleware fn to app

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
 
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
