c97b1e7bd3fc95933262ba92711e97e79b49cf11
SaraToth <saratoth101@gmail.com>
2025-06-17 17:47:46 +0900

    Feat: Add a 404 handler for non-existing routes

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
 
+//404 Handler for non existing pages
+app.use((req, res, next) => {
+    res.status(404).send("This page does not exist");
+});
+
+// Error Middleware Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(err.statusCode || 500).send(err.message);
