4e21457161690569a1edfebd709e933b9860d5b1
SaraToth <saratoth101@gmail.com>
2025-06-17 17:58:25 +0900

    Feat: Create views for index routes

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,13 @@
 function getHome(req, res) {
-    res.send("Home");
+    res.render("index", { title: "Home" });
 };
 
 function getAbout(req, res) {
-    res.send("About");
+    res.render("index", { title: "About"});
 };
 
 function getContact(req, res) {
-    res.send("Contact");
+    res.render("index", { title: "Contact"});
 };
 
 module.exports = { getHome, getAbout, getContact };
